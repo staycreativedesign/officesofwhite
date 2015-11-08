33b72958d4b5834dd2ded65fd1fe082099f62ba3
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-11-07 21:50:34 -0600

    +users - added user personal information

---
+++ db/migrate/20151107230723_add_personal_info_to_users.rb
@@ -0,0 +1,12 @@
+class AddPersonalInfoToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :suffix, :string
+    add_column :users, :middle_name, :string
+    add_column :users, :phone_number, :integer
+    add_column :users, :client_driver_license, :string
+    add_column :users, :client_social_security, :integer
+    add_column :users, :client_dob, :date
+  end
+end
