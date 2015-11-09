98f2b976debb7b33106f73e062588ab3b5626da8
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-11-08 18:01:48 -0600

    +user - added date of birth helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,10 @@ module UsersHelper
      ['Mrs.','mrs.']
    ]
   end
+
+  def date_of_birth_options
+    {
+    start_year: 1901, end_year: 2010, order: [:month, :day, :year], prompt: { day: 'Select day', month: 'Select month', year: 'Select year' }
+    }
+  end
 end
