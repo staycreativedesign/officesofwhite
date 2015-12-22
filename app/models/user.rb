d9735c6efe44dc4e96e737b3833ef58f7314550b
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-12-22 00:41:55 -0600

    added personal information

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessor :phone_number_1, :phone_number_2, :phone_number_3, :other_phone_number_1, :other_phone_number_2, :other_phone_number_3, :client_ss_1, :client_ss_2, :client_ss_3
+  attr_accessor :phone_number_1, :phone_number_2, :phone_number_3,
+                :other_phone_number_1, :other_phone_number_2,
+                :other_phone_number_3, :ss_1, :ss_2, :ss_3
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,10 +10,14 @@ class User < ActiveRecord::Base
   validates_presence_of :last_name
   validates_presence_of :suffix
   validates_presence_of :phone_number
-  validates_presence_of :client_social_security
-  validates_presence_of :client_driver_license
-  validates_presence_of :client_dob
-  validates_presence_of :client_registration_ip
-
+  validates_presence_of :social_security
+  validates_presence_of :driver_license
+  validates_presence_of :dob
+  validates_presence_of :registration_ip
+  validates_presence_of :home_address
+  validates_presence_of :city
+  validates_presence_of :zipcode
+  validates_presence_of :time_at_address_months
+  validates_presence_of :time_at_address_years
 end
 
