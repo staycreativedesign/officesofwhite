715ff49422e20adf4b907c2b9bc1d379745defdc
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-12-29 01:45:46 -0600

    users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessor :phone_number_1, :phone_number_2, :phone_number_3,
                 :other_phone_number_1, :other_phone_number_2,
-                :other_phone_number_3, :ss_1, :ss_2, :ss_3
+                :other_phone_number_3, :ss_1, :ss_2, :ss_3, :employment_phone_1, :employment_phone_2, :employment_phone_3
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -14,11 +14,24 @@ class User < ActiveRecord::Base
   validates_presence_of :driver_license
   validates_presence_of :dob
   validates_presence_of :registration_ip
-
   validates_presence_of :home_address
+  validates_presence_of :residence_type
+  validates_presence_of :household_payment
+  validates_presence_of :state
   validates_presence_of :city
   validates_presence_of :zipcode
   validates_presence_of :time_at_address_months
   validates_presence_of :time_at_address_years
+  validates_presence_of :employer_name
+  validates_presence_of :occupation
+  validates_presence_of :employment_address
+  validates_presence_of :employment_city
+  validates_presence_of :employment_state
+  validates_presence_of :employment_zipcode
+  validates_presence_of :employment_phone
+  validates_presence_of :employment_gross_income
+  validates_presence_of :employment_years
+  validates_presence_of :employment_months
+
 end
 
