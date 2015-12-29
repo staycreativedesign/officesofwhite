bb74490b3842ef2e78fe472b60bbd63aca26fb46
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-12-29 01:54:11 -0600

    added address, residence, employer

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,26 @@ describe User do
   it { should validate_presence_of(:last_name) }
   it { should validate_presence_of(:suffix) }
   it { should validate_presence_of(:phone_number) }
-  it { should validate_presence_of(:client_social_security) }
-  it { should validate_presence_of(:client_driver_license) }
-  it { should validate_presence_of(:client_dob) }
-  it { should validate_presence_of(:client_registration_ip) }
+  it { should validate_presence_of(:social_security) }
+  it { should validate_presence_of(:driver_license) }
+  it { should validate_presence_of(:dob) }
+  it { should validate_presence_of(:registration_ip) }
+  it { should validate_presence_of(:home_address) }
+  it { should validate_presence_of(:city) }
+  it { should validate_presence_of(:state) }
+  it { should validate_presence_of(:zipcode) }
+  it { should validate_presence_of(:time_at_address_months) }
+  it { should validate_presence_of(:time_at_address_years) }
+  it { should validate_presence_of(:residence_type) }
+  it { should validate_presence_of(:household_payment) }
+  it { should validate_presence_of(:employer_name) }
+  it { should validate_presence_of(:occupation) }
+  it { should validate_presence_of(:employment_address) }
+  it { should validate_presence_of(:employment_city) }
+  it { should validate_presence_of(:employment_state) }
+  it { should validate_presence_of(:employment_zipcode) }
+  it { should validate_presence_of(:employment_phone) }
+  it { should validate_presence_of(:employment_gross_income) }
+  it { should validate_presence_of(:employment_years) }
+  it { should validate_presence_of(:employment_months) }
 end
