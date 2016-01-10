cbd174adb1b3c1859fa6b3c9009511f46d499476
Gustavo ParesY <gustavoanalytics@gmail.com>
2016-01-10 04:45:33 -0600

    added autotabs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,34 @@
 
 
 $( document ).ready(function() {
-  $('.number').autotab('number');
-  $('.alphanumeric').autotab('alphanumeric');
-  $('.ssn').autotab('number');
+  $('#user_phone_number_1').autotab({ format: 'numeric', target: '#user_phone_number_2', previous:
+  '#user_last_name' });
+  $('#user_phone_number_2').autotab({ format: 'numeric', target: '#user_phone_number_3', previous:
+  '#user_phone_number_1' });
+  $('#user_phone_number_3').autotab({ format: 'numeric', target: '#user_other_phone_number_1', previous:
+  '#user_phone_number_2' });
+    $('#user_other_phone_number_1').autotab({ format: 'numeric', target: '#user_other_phone_number_2', previous:
+  '#user_phone_number_3' });
+  $('#user_other_phone_number_2').autotab({ format: 'numeric', target: '#user_other_phone_number_3', previous:
+  '#user_other_phone_number_1' });
+  $('#user_other_phone_number_3').autotab({ format: 'numeric', target: '#user_driver_license', previous:
+  '#user_other_phone_number_2' });
+  $('#user_driver_license').autotab({ format: 'alphanumeric', target: '#user_ss_1', previous:
+  '#user_other_phone_number_3' });
+  $('#user_ss_1').autotab({ format: 'numeric', target: '#user_ss_2', previous:
+  '#user_driver_license' });
+  $('#user_ss_2').autotab({ format: 'numeric', target: '#user_ss_3', previous:
+  '#user_ss_1' });
+  $('#user_ss_3').autotab({ format: 'numeric', target: '#user_dob_2i', previous:
+  '#user_ss_2' });
+  $('#user_zipcode').autotab({ format: 'numeric', target: '#user_time_at_address_months', previous:
+  '#user_state' });
+  $('#user_employment_zipcode').autotab({ format: 'numeric', target: '#user_employment_phone_1', previous:
+  '#user_employment_state' });
+  $('#user_employment_phone_1').autotab({ format: 'numeric', target: '#user_employment_phone_2', previous:
+  '#user_employment_zipcode' });
+  $('#user_employment_phone_2').autotab({ format: 'numeric', target: '#user_employment_phone_3', previous:
+  '#user_employment_phone_1' });
+  $('#user_employment_phone_3').autotab({ format: 'numeric', target: '#user_employment_months', previous:
+  '#user_employment_phone_2' });
 });
