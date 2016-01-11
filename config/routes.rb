9dc8ee5600cc5cfbbc7b382a251685517d2a069e
Gustavo ParesY <gustavoanalytics@gmail.com>
2016-01-11 16:27:24 -0600

    removed users controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
     :controllers => { :registrations => "registrations" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
   # You can have the root of your site routed with "root"
   root 'pages#index'
 
