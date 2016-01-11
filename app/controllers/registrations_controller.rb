6959756a54899405f70d24d3505fc9a7712f6301
Gustavo ParesY <gustavoanalytics@gmail.com>
2016-01-10 22:56:24 -0600

    added create

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,17 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_filter :configure_permitted_parameters
+
   def new
     @header = "bg-registration"
     super
   end
+
+  def create
+    super
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit! }
+  end
+
 end
