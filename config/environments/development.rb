fe253b0bf88b4bb6ab66c3d931861b5948fa32de
Gustavo Pares <gustavoanalytics@gmail.com>
2016-01-19 14:23:31 -0600

    added env variables for mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method :letter_opener
+  config.action_mailer.delivery_method :smtp
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -38,6 +38,12 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => "smtp.mailgun.org",
+    :port => 587,
+    :domain => "MYDOMAIN.mailgun.org",
+    :user_name => "postmaster@MYDOMAIN.mailgun.org",
+  }
 end
