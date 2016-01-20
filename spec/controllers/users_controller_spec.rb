6bbe960f73a936f1e36897e1bfd09b5f53414b41
Gustavo Pares <gustavoanalytics@gmail.com>
2016-01-20 02:51:34 -0600

    added before do, still missing expect for failed user

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,22 +3,24 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   describe "POST create" do
     context "user fills out form correctly" do
-      it "creates a new user" do
+      before do
         post :create, user: Fabricate.attributes_for(:user)
+      end
+      it "creates a new user" do
         expect(User.count).to equal(1)
       end
       it "sends an email" do
-        post :create, user: Fabricate.attributes_for(:user)
-        expect(ActionMailer::Base.deliveries.count).to equal(2)
+        expect(ActionMailer::Base.deliveries).to_not be_empty
       end
     end
     context "user does not fill out form correctly" do
-      it "does not create user" do
+      before do
         post :create, user: Fabricate.attributes_for(:user, first_name: nil)
+      end
+      it "does not create user" do
         expect(User.count).to equal(0)
       end
       it "redirects to new" do
-        post :create, user: Fabricate.attributes_for(:user, first_name: nil)
         expect(response).to render_template(:new)
       end
     end
