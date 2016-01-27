bc4920944c6b7512fc90c85399b9f198e6b98d08
Gustavo Pares <gustavoanalytics@gmail.com>
2016-01-27 15:08:25 -0600

    added testing for logging user in

--- spec/features/login_user_spec.rb
+++ spec/features/login_user_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 feature 'login user' do
+  let(:user) { Fabricate(:user) }
   scenario '+valid - User signs in correctly' do
     visit login_path
+    fill_in("user_email", with: user.email)
+    fill_in("user_password", with: user.password)
   end
 end
