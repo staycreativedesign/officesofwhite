57c5b4b1bfcd14a468fed69c554aaff5caa485f5
Gustavo Pares <gustavoanalytics@gmail.com>
2016-02-01 23:26:13 -0600

    fixed user logging in test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,8 +9,7 @@ require 'capybara/poltergeist'
 require 'sidekiq/testing'
 require 'capybara/rspec'
 require 'database_cleaner'
-Capybara.default_driver = :poltergeist
-#Capybara.default_driver = :selenium
+Capybara.default_driver = :selenium
 #Capybara.javascript_driver = :poltergeist
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -35,50 +34,8 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  config.include Capybara::DSL
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
   config.use_transactional_fixtures = false
 
-
-	config.before(:each) do
-		DatabaseCleaner.strategy = :transaction
-	end
-
-	config.before(:each, type: :feature) do
-		# :rack_test driver's Rack app under test shares database connection
-		# with the specs, so continue to use transaction strategy for speed.
-		driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
-
-		if !driver_shares_db_connection_with_specs
-			# Driver is probably for an external browser with an app
-			# under test that does *not* share a database connection with the
-			# specs, so use truncation strategy.
-			DatabaseCleaner.strategy = :truncation
-		end
-	end
-
-	config.before(:each) do
-		DatabaseCleaner.start
-	end
-
-	config.append_after(:each) do
-		DatabaseCleaner.clean
-	end
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 end
 Shoulda::Matchers.configure do |config|
