6bc00787d8ed2ba4810aec0b342906974a81e026
Gustavo Pares <gustavoanalytics@gmail.com>
2016-02-02 21:54:05 -0600

    added helper, updated spec macros, first_steps_controller_spec

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -7,13 +7,15 @@ def set_current_user(user=nil)
   session[:user_id] = (user || Fabricate(:user)).id
 end
 
+def set_current_user_feature(user=nil)
+  page.set_rack_session(user_id: user.id)
+end
 def sign_in(a_user=nil)
   user = a_user || Fabricate(:user)
-  visit new_user_session_path
+  visit login_path
   fill_in "Email", with: user.email
   fill_in "Password", with: user.password
   click_button "Log in"
-  visit new_user_first_step_path(user)
 end
 def clear_current_user
   session[:user_id] = nil
