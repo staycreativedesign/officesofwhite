59de3e9c3f5828d50c7cc39b0bf7a34f8411edba
Gustavo Pares <gustavoanalytics@gmail.com>
2016-02-05 14:28:38 -0600

    logging out spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
 
-Rspece.describe SessionsController, type: :controller do
+RSpec.describe SessionsController, type: :controller do
+  describe "GET destroy" do
+    context "user clicks logout" do
+      before do
+        jim = Fabricate(:user, first_name: "jim")
+        set_current_user(jim)
+      end
+      it "sets the session[:user_id] to nil" do
+        get :destroy
+        expect(session[:user_id]).to be_nil
+      end
+      it "redirects to root_path" do
+        get :destroy
+        expect(response).to redirect_to root_path
+      end
+    end
 
+  end
 end
 
