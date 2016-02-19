b6f6cc07bc7d93f3d81bf5b50b6f41ddf55afbea
Gustavo Pares <gustavoanalytics@gmail.com>
2016-02-18 21:04:43 -0600

    added post create tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,20 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-  describe "GET destroy" do
-    context "user clicks logout" do
-      before do
-        jim = Fabricate(:user, first_name: "jim")
-        set_current_user(jim)
+  describe "POST create" do
+    let(:jim) { Fabricate(:user, first_name: "jim") }
+
+    context "with valid credentials" do
+      before { post :create, email: jim.email, password: jim.password}
+      it { is_expected.to redirect_to(steps_path)  }
+
+      it "sets session" do
+        expect(session[:user_id]).to eq(jim.id)
       end
-      it "sets the session[:user_id] to nil" do
-        get :destroy
+
+      it "sets flash notice" do
+        expect(flash[:notice]).to be_present
+      end
+    end
+
+    context "width invalid credentials" do
+      before { post :create, email: jim.email, password: "wrong" }
+      it { is_expected.to render_template(:new) }
+    end
+  end
+
+  describe "DELETE destroy" do
+    subject(:response) { delete :destroy }
+
+    context "user is logged in" do
+      let(:jim) { Fabricate(:user, first_name: "jim") }
+      before { set_current_user(jim) }
+
+      it "sets session[:user_id] to nil" do
+        delete :destroy
         expect(session[:user_id]).to be_nil
       end
-      it "redirects to root_path" do
-        get :destroy
-        expect(response).to redirect_to root_path
+      context "user is logged out" do
+        it { is_expected.to redirect_to(root_path)  }
       end
+
     end
   end
 end
