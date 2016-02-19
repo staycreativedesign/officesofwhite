48629c254cec4e8d13ea759eda1da96b2f9e8c5b
Gustavo Pares <gustavoanalytics@gmail.com>
2016-02-18 21:05:08 -0600

    added redirect to steps_path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,11 @@
 class SessionsController < ApplicationController
   before_action :set_image_header, only: [:create, :new]
-  def new
-  end
-
   def create
-    user = User.where(email: params[:email]).first
+    user = User.find_by(email: params[:email])
     if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:notice] = "You have logged in correctly"
-      redirect_to root_path
+      redirect_to steps_path
     else
       flash[:error] = "Please check your email or or password."
       render :new
