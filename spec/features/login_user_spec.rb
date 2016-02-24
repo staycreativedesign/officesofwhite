403ea765ec3cb7c49d58c2e61439f1e43dd61921
Gustavo Pares <gustavoanalytics@gmail.com>
2016-02-23 23:07:40 -0600

    feature test - user logging in when approved

--- spec/features/login_user_spec.rb
+++ spec/features/login_user_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 feature 'login user' do
-  let!(:user) { Fabricate(:user) }
+  let!(:user) { Fabricate(:user, approved: true, step_number: 1) }
 
-  scenario '+valid - User signs in correctly' do
+  scenario '+valid - User signs in correctly and is approved' do
     visit login_path
     fill_in("email", with: user.email)
     fill_in("password", with: user.password)
     click_button("Log in")
-    expect(page).to have_current_path(root_path)
+    expect(page).to have_current_path(steps_path)
   end
 end
