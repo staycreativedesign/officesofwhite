bd94ded5fc8c59b7017f689eb6cd3e615bf96b4c
Gustavo Pares <gustavoanalytics@gmail.com>
2016-02-26 00:33:55 -0600

    added disclosure statement

--- app/models/concerns/documents.rb
+++ app/models/concerns/documents.rb
@@ -3,10 +3,10 @@ module Documents
   included do
 
     #STEP 1
-    STEP_ONE_DOCUMENTS = [:letter_of_representation, :payment_verification, :disclosure_statement]
+    STEP_ONE_DOCUMENTS = [:service_agreement, :letter_of_representation, :disclosure_statement]
     belongs_to :disclosure_statement, class_name: "Document"
     belongs_to :letter_of_representation, class_name: "Document"
-    belongs_to :payment_verification, class_name: "Document"
+    belongs_to :service_agreement, class_name: "Document"
     accepts_nested_attributes_for(*STEP_ONE_DOCUMENTS)
 
   end
