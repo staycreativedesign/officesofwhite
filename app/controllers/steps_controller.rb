3751589821d0cc1f613cb31b3e65d14fa6fb885a
Gustavo Pares <gustavoanalytics@gmail.com>
2016-02-27 10:20:24 -0600

    added step_three method

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -10,12 +10,16 @@ class StepsController < ApplicationController
       redirect_to waiting_for_approval_path
     when 1
       step_one
+    when 2
+      step_two
+    when 3
+      step_three
     end
   end
   #REFACTOR
   def step_one
     @header = "bg-index"
-    find_documents_for_step(User::STEP_ONE_DOCUMENTS)
+    find_documents_for_step(current_user.set_documents)
     respond_to do |format|
       format.html
       format.pdf do
@@ -25,12 +29,18 @@ class StepsController < ApplicationController
     end
   end
 
+  def step_three
+    @header = "bg-index"
+    find_documents_for_step(current_user.set_documents)
+  end
+
   def upload_documents
     if @user.update_attributes(step_params)
+      @user.update_attributes(step_number: 2)
+      binding.pry
       redirect_to waiting_for_approval_path, alert: "Your documents were uploaded"
-      @user.update_attributes(step_number: 0)
     else
-      render :index, alert: "Some errors occured please re-upload your documents"
+      redirect_to :index, alert: "Some errors occured please re-upload your documents"
     end
   end
 
