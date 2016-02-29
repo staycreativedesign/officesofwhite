659ae86822af17c8095197a7fbbb0c96ffdd6307
Gustavo Pares <gustavoanalytics@gmail.com>
2016-02-28 23:31:07 -0600

    fixed error

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -29,6 +29,11 @@ class StepsController < ApplicationController
     end
   end
 
+  def step_two
+    @header = "bg-index"
+    find_documents_for_step(current_user.set_documents)
+  end
+
   def step_three
     @header = "bg-index"
     find_documents_for_step(current_user.set_documents)
@@ -37,7 +42,6 @@ class StepsController < ApplicationController
   def upload_documents
     if @user.update_attributes(step_params)
       @user.update_attributes(step_number: 2)
-      binding.pry
       redirect_to waiting_for_approval_path, alert: "Your documents were uploaded"
     else
       redirect_to :index, alert: "Some errors occured please re-upload your documents"
@@ -52,7 +56,8 @@ class StepsController < ApplicationController
   def step_params
     hash = Hash.new
     @user.set_documents.each do |document|
-      hash[:"#{document}_attributes"] = [:file]
+      hash[:"#{document}_attributes"] = [:file, :user_id]
+      binding.pry
     end
     params.require(:user).permit(hash)
   end
