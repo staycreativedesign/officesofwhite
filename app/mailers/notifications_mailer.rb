1d19919ecb14c907c8e46cf2d6c2529d9daa280f
Gustavo Pares <gustavoanalytics@gmail.com>
2016-03-04 15:31:43 -0600

    added new send documents with helper

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,4 +1,5 @@
 class NotificationsMailer < ApplicationMailer
+  helper :mailer
   def new_user_registration(user)
     @user = user
     attachments['xxx.pdf'] = WickedPdf.new.pdf_from_string(
@@ -7,9 +8,13 @@ class NotificationsMailer < ApplicationMailer
     mail to: "gustavoanalytics@gmail.com", subject: "This is a test email", body: "This is your registration please keep it for your records."
   end
 
-  def send_admin_documents(documents, user)
+  def send_admin_documents(files, user)
     @user = user
-    @documents = documents
-    mail to: "gustavoanalytics@gmaim.com", subject: "#{@user} has sent you an email"
+    @files = files
+    mail to: "gustavoanalytics@gmaim.com", subject: "#{full_name} has sent you an email"
+  end
+
+  def full_name
+    @user.first_name + " " + @user.last_name
   end
 end
