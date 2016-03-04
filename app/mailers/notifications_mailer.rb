42dfbe9c78abadab7cb8ad03392fdd928c966877
Gustavo Pares <gustavoanalytics@gmail.com>
2016-03-04 01:20:39 -0600

    added send docs to admin

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -6,4 +6,10 @@ class NotificationsMailer < ApplicationMailer
     )
     mail to: "gustavoanalytics@gmail.com", subject: "This is a test email", body: "This is your registration please keep it for your records."
   end
+
+  def send_admin_documents(documents, user)
+    @user = user
+    @documents = documents
+    mail to: "gustavoanalytics@gmaim.com", subject: "#{@user} has sent you an email"
+  end
 end
