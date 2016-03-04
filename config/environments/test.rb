6cecfa07279525b5d33b7f6eb924c53a42595446
Gustavo Pares <gustavoanalytics@gmail.com>
2016-03-04 02:50:36 -0600

    added letter opener to delivery method

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000  }
 
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.smtp_settings = {
     :authentication => :plain,
     :address => "smtp.mailgun.org",
