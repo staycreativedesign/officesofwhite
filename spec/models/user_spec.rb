72598b325c95ab97cdb2a810d1ce38d1a7cbecce
Gustavo Pares <gustavoanalytics@gmail.com>
2016-03-07 16:07:50 -0600

    added receipts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe User do
   subject { Fabricate(:user) }
+  it { should have_many(:receipts) }
   it { should validate_presence_of(:first_name) }
   it { should validate_presence_of(:last_name) }
   it { should validate_presence_of(:suffix) }
