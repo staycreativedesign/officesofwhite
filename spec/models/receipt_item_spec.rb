9272a20551eb18f0f1b8bbb23bbb3e948c233700
Gustavo Pares <gustavoanalytics@gmail.com>
2016-03-09 12:22:27 -0600

    receipt item specs complete

--- spec/models/receipt_item_spec.rb
+++ spec/models/receipt_item_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 describe ReceiptItem do
   let(:jim) { Fabricate(:user, first_name: "Jim") }
   let(:receipt) { Fabricate(:receipt, user_id: jim.id) }
-  let(:receipt_item) { Fabricate(:receipt_item, receipt_id: receipt.id) }
+  subject { Fabricate(:receipt_item, receipt_id: receipt.id, name: "Blue Horse shoe", value: "5.00") }
+  it { should belong_to(:receipt) }
+  it { is_expected.to have_attributes(:name => "Blue Horse shoe", value: "5.00")  }
 end
