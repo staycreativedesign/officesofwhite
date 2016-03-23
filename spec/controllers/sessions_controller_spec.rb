c4db336af5b708f015c11557bd2956037586b021
Gustavo Pares <gustavoanalytics@gmail.com>
2016-03-23 13:35:21 -0500

    added test for redirect to admin panel if user is admin

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
   describe "POST create" do
-    let(:jim) { Fabricate(:user, first_name: "jim") }
+    let(:admin) { false }
+    let(:attrs) { {} }
+    let(:jim) { Fabricate :user, attrs.merge( admin: admin )}
 
     context "with valid credentials" do
       before { post :create, email: jim.email, password: jim.password}
       it { is_expected.to redirect_to(steps_path)  }
+
 
       it "sets session" do
         expect(session[:user_id]).to eq(jim.id)
@@ -14,6 +17,11 @@ RSpec.describe SessionsController, type: :controller do
 
       it "sets flash notice" do
         expect(flash[:notice]).to be_present
+      end
+
+      context "with admin credentials" do
+        let(:admin) { true }
+        it { is_expected.to redirect_to(admin_panel_index_path) }
       end
     end
 
