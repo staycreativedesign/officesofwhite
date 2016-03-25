bd7a62964e11211a01b7ec95bb83dcf652d2339f
Gustavo Pares <gustavoanalytics@gmail.com>
2016-03-25 13:17:51 -0500

    added log in and incorrect login spec

--- spec/controllers/admin/panel_controller_spec.rb
+++ spec/controllers/admin/panel_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Admin::PanelController, type: :controller do
+  describe "GET index" do
+    render_views
+    subject(:response) { get :index }
 
+    let(:admin) { false }
+    let(:jim) { Fabricate :user, admin: admin }
+
+    context "admin is logged in to panel" do
+      before { set_current_user(jim) }
+      let(:admin) { true }
+      it { is_expected.to render_template(:index) }
+    end
+
+    context "user is not admin" do
+      before { set_current_user(jim) }
+      let(:admin) { false }
+      it { is_expected.to redirect_to(login_path) }
+    end
+  end
 end
