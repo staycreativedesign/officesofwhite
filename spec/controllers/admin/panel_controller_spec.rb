907dd459355a7cd376d85aab30e8261f3fc3e8b4
Gustavo Pares <gustavoanalytics@gmail.com>
2016-03-28 15:28:53 -0500

    added assigns @users passing, added @total_payments_received passing

--- spec/controllers/admin/panel_controller_spec.rb
+++ spec/controllers/admin/panel_controller_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Admin::PanelController, type: :controller do
+  render_views
   describe "GET index" do
-    render_views
     subject(:response) { get :index }
+    subject(:make_request) { get :index }
 
     let(:admin) { false }
     let(:jim) { Fabricate :user, admin: admin }
@@ -12,6 +13,24 @@ RSpec.describe Admin::PanelController, type: :controller do
       before { set_current_user(jim) }
       let(:admin) { true }
       it { is_expected.to render_template(:index) }
+
+      it "assigns @users" do
+        make_request
+        users = assigns[:users]
+        expect(users.count).to eql 1
+        expect(users.first.id).to eql jim.id
+      end
+
+      it "assigns @total_payments_received" do
+        2.times { create_paid_receipt_item(jim, jim.receipts.first, "20.00", true) }
+        make_request
+        total_payments_received = assigns[:total_payments_received]
+        binding.pry
+        expect(ReceiptItem.count).to eql 2
+        expect(total_payments_received).to eql 40.00
+      end
+
+      it "assigns @total_payments_pending"
     end
 
     context "user is not admin" do
