cd9f40e3d5bb5bcba4d6b9bf15ab8c8485f8b7cd
Gustavo Pares <gustavoanalytics@gmail.com>
2016-03-28 13:15:39 -0500

    added total payments received

--- app/controllers/admin/panel_controller.rb
+++ app/controllers/admin/panel_controller.rb
@@ -1,6 +1,13 @@
 class Admin::PanelController < AdminsController
   def index
     @users = User.all
+    @total_payments_received = total_payments_received
+  end
+
+  private
+
+  def total_payments_received
+    ReceiptItem.select { |obj| obj.paid? }
   end
 
 end
