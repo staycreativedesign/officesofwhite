0f05dd34f60a732af7f2e9243dbef7f13efcad5d
Gustavo Pares <gustavoanalytics@gmail.com>
2016-03-28 17:13:37 -0500

    added total_pending_payments total_payments_received macros

--- app/controllers/admin/panel_controller.rb
+++ app/controllers/admin/panel_controller.rb
@@ -2,12 +2,21 @@ class Admin::PanelController < AdminsController
   def index
     @users = User.all
     @total_payments_received = total_payments_received
+    @total_pending_payments = total_pending_payments
   end
 
   private
 
   def total_payments_received
-    ReceiptItem.select { |obj| obj.paid? }
+    receipts = ReceiptItem.select { |obj| obj.paid? }
+    total_value = receipts.map { |rec| rec.value.to_f }
+    total_value.reduce(:+)
+  end
+
+  def total_pending_payments
+    receipts = ReceiptItem.select { |obj| !obj.paid? }
+    total_value = receipts.map { |rec| rec.value.to_f }
+    total_value.reduce(:+)
   end
 
 end
