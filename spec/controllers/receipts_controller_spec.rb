c7f70cb2395be757f384cf795e889f81853eefb1
Gustavo Pares <gustavoanalytics@gmail.com>
2016-05-06 15:24:44 -0500

    rubocop - fix "" ''

--- spec/controllers/receipts_controller_spec.rb
+++ spec/controllers/receipts_controller_spec.rb
@@ -1,31 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe ReceiptsController, type: :controller do
-  describe "GET index" do
+  describe 'GET index' do
     subject { get :index, user_id: jim.id }
 
-    let(:jim) { Fabricate :user, approved: true, first_name: "Jim" }
+    let(:jim) { Fabricate :user, approved: true, first_name: 'Jim' }
 
-    context "user is logged in" do
+    context 'user is logged in' do
       before do
         set_current_user(jim)
         create_receipt(jim)
       end
-
-      it "renders the index template" do
+      it 'renders the index template' do
         expect(subject).to render_template(:index)
       end
-      it "shows all receipts for current_user" do
+      it 'shows all receipts for current_user' do
         expect(jim.receipts.count).to eq(1)
       end
     end
   end
 
-  describe "GET show" do
-    let(:jim) { Fabricate :user, approved: true, first_name: "Jim" }
+  describe 'GET show' do
+    let(:jim) { Fabricate :user, approved: true, first_name: 'Jim' }
 
-    context "user is logged in" do
-
+    context 'user is logged in' do
       before do
         set_current_user(jim)
         create_receipt(jim)
