a6657d4c76249cd4623ae82029f8083a801da611
Gustavo Pares <gustavoanalytics@gmail.com>
2016-05-10 18:07:18 -0500

    added show

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,15 @@
 class Admin::UsersController < AdminsController
   def index
     @users = User.all
-    {
-    }
   end
 
+  def show
+    find_user
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
 end
