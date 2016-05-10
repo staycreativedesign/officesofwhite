83afdcee6d519f2b4d36bb92e59803e8a589c33b
Gustavo Pares <gustavoanalytics@gmail.com>
2016-05-10 15:00:32 -0500

    made rubocop happy

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,34 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe "POST create" do
-    context "user fills out form correctly" do
+  describe 'POST create' do
+    context 'user fills out form correctly' do
       before do
-        post :create, user: Fabricate.attributes_for(:user, phone_number_1: "305", phone_number_2: "619", phone_number_3: "3724")
+        post :create, user: Fabricate.attributes_for(:user,
+                                                     phone_number_1: '305',
+                                                     phone_number_2: '619',
+                                                     phone_number_3: '3724')
       end
-      it "creates a new user" do
+      it 'creates a new user' do
         expect(User.count).to equal(1)
       end
-      it "sends an email" do
+      it 'sends an email' do
         expect(Sidekiq::Extensions::DelayedMailer.jobs.count).to eq(0)
       end
 
-      it "formats the phone number" do
-        expect(User.last.phone_number).to eq("305 - 619 - 3724")
+      it 'formats the phone number' do
+        expect(User.last.phone_number).to eq('305 - 6t19 - 3724')
       end
     end
-    context "user does not fill out form correctly" do
+    context 'user does not fill out form correctly' do
       before do
         Sidekiq::Extensions::DelayedMailer.jobs.clear
         post :create, user: Fabricate.attributes_for(:user, first_name: nil)
       end
-      it "does not create user" do
+      it 'does not create user' do
         expect(User.count).to equal(0)
       end
-      it "redirects to new" do
+      it 'redirects to new' do
         expect(response).to render_template(:new)
       end
-      it "does not send email" do
+      it 'does not send email' do
         expect(Sidekiq::Extensions::DelayedMailer.jobs.count).to eq(0)
       end
     end
