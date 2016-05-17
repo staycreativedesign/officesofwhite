c5bc66ea65623479edfac6dc85cc02118fa06bff
Gustavo Pares <gustavoanalytics@gmail.com>
2016-05-17 12:49:52 -0500

    updated sign_in to include admin, selenium_trigger

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -10,8 +10,8 @@ end
 def set_current_user_feature(user=nil)
   page.set_rack_session(user_id: user.id)
 end
-def sign_in(a_user=nil)
-  user = a_user || Fabricate(:user)
+def sign_in(a_user = nil, admin = false)
+  user = a_user || Fabricate(:user, admin: admin)
   visit login_path
   fill_in "Email", with: user.email
   fill_in "Password", with: user.password
@@ -20,6 +20,14 @@ end
 
 def clear_current_user
   session[:user_id] = nil
+end
+
+def selenium_trigger_click(element_name)
+  if Capybara.default_driver == :selenium
+    page.find(element_name).click
+  else
+    page.find(element_name).trigger('click')
+  end
 end
 
 def create_receipt(a_user=nil)
