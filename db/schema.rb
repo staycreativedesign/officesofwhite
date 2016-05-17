528c1ac02f14efed61113ae31b9a4eeb5fca7add
Gustavo Pares <gustavoanalytics@gmail.com>
2016-05-17 03:00:01 -0500

    added new schema with phone and fax

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516200000) do
+ActiveRecord::Schema.define(version: 20160517074301) do
 
   create_table "documents", force: :cascade do |t|
     t.integer  "user_id",                     null: false
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20160516200000) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "phone_number"
+    t.string   "fax_number"
   end
 
   create_table "receipt_items", force: :cascade do |t|
