e4c7ac28f0fb04fbdb249b2ff034f9aa2f2f4e02
Gustavo Pares <gustavoanalytics@gmail.com>
2016-05-16 22:59:01 -0500

    added CRUD

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,8 +7,33 @@ class Admin::UsersController < AdminsController
     find_user
   end
 
+  def edit
+    find_user
+  end
+
+  def update
+    find_user
+    if @user.update_attributes(user_params)
+      redirect_to admin_user_path(@user)
+      flash[:notice] = 'User updated'
+    else
+      render :edit
+      flash[:notice] = 'User not updated'
+    end
+  end
+
+  def destroy
+    find_user
+    @user.destroy
+    redirect_to admin_users_path
+    flash[:notice] = "User has been deleted"
+  end
+
   private
 
+  def user_params
+    params.require(:user).permit!
+  end
   def find_user
     @user = User.find(params[:id])
   end
