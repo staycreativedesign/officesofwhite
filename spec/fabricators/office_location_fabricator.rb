12a6381b96d9698ba34d4b5202581ea4ef74a5b5
Gustavo Pares <gustavoanalytics@gmail.com>
2016-05-19 12:17:10 -0500

    added state_abbr to state

--- spec/fabricators/office_location_fabricator.rb
+++ spec/fabricators/office_location_fabricator.rb
@@ -2,7 +2,7 @@ Fabricator(:office_location) do
   name { Faker::Company.name }
   address { Faker::Address.street_address }
   city { Faker::Address.city }
-  state { Faker::Address.state }
+  # state { Faker::Address.state_abbr }
   zipcode { Faker::Address.zip }
   phone_number {Faker::PhoneNumber.phone_number}
   fax_number { Faker::PhoneNumber.phone_number }
